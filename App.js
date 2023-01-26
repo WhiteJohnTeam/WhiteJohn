68e0feff9ccbd288f27b7a103767694a4a8d087b
nicolas.franco <nicolas.franco@etu.uca.fr>
2023-01-26 19:37:22 +0100

    setting options structured

--- App.js
+++ App.js
@@ -1,58 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { Component } from 'react';
-import { StyleSheet, Text, Image, View, TouchableOpacity, SafeAreaView} from 'react-native';
+import { StyleSheet, Text, Image, View, TouchableOpacity, SafeAreaView, Switch} from 'react-native';
 //import { SafeAreaView, Button } from 'react-native-web';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.middle}>
-        <Image source={require('./assets/logo_WhiteJohn.png')}/>
-      <Text>Your BlackJack Companion</Text>
-      <StatusBar style="auto" />
-      </View>
+    <SafeAreaView style={{flex: 1, margin: 5}}>
       
-      <View style={styles.down}>
-        <TouchableOpacity style={styles.buttonForm}>
-          <Image source={require('./assets/playButton.png')} style={styles.buttonImage}/>      
-        </TouchableOpacity>
+      {/* TITLE */}
+      <View style={{position: 'absolute', top: 10, alignSelf: 'center'}}>
+        <Text style={styles.title}>SETTINGS</Text>
       </View>
-      
+
+      {/* SETTING ICON 
+      <View style={{position: 'absolute', top: 20, right: 20, marginTop: 15}}>
+        <Icon name='settings' size={45}></Icon>
+      </View>*/}
+
+      <View style={styles.options}>
+        <View style={styles.settingRow}>
+          <Icon name='globe-outline' size={65}></Icon>
+          <Text style={{fontSize: 30, fontWeight: 'bold'}}>Language</Text>
+          <Switch></Switch>
+        </View>
+
+        <View style={styles.settingRow}>
+          <View style={styles.iconDecoration}>
+            <Icon name='moon' size={50}></Icon>
+          </View>
+          <Text style={{fontSize: 30, fontWeight: 'bold'}}>Dark Mode</Text>
+          <Switch></Switch>
+        </View>
+
+        <View style={styles.settingRow}>
+          <Icon name='information-circle-outline' size={65}></Icon>
+          <Text style={{fontSize: 30, fontWeight: 'bold'}}>Language</Text>
+          <Switch></Switch>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  title: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    fontSize: 40,
+    fontWeight: 'bold',
+    fontFamily: 'sans-serif-medium',
+    margin: 20
   },
-  middle: {
-    flex: 1,
-    backgroundColor: '#fff',
+
+  iconDecoration: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'white',
+    borderRadius: 30,
+    borderWidth: 3,
     alignItems: 'center',
-    justifyContent: 'flex-end',
   },
-  down: {
-    flex: 1,
-    backgroundColor: '#fff',
+
+  settingRow: {
+    flex: 2,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between'
   },
-  buttonForm: {
-    width: 80,
-    height: 80,
-    borderColor: 'black',
-    borderWidth: 5,
-    borderRadius: 40,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonImage: {
-    height: 20,
-    width: 20,
+
+  options: {
+    //backgroundColor: 'grey',
+    alignSelf: 'center',
+    height: 300,
+    width: 360,
+    marginTop: 150
   }
 });
 
