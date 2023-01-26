9f0cef76bf6693a9996df28ca46a3a4e1a7839dc
lou.broda <lou.broda@etu.uca.fr>
2023-01-26 16:19:46 +0100

    push start home_page

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { StyleSheet, Text, Image, View, TouchableOpacity, SafeAreaView} from 'react-native';
+//import { SafeAreaView, Button } from 'react-native-web';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>White John</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.middle}>
+        <Image source={require('./assets/logo_WhiteJohn.png')}/>
+      <Text>Your BlackJack Companion</Text>
       <StatusBar style="auto" />
-    </View>
+      </View>
+      
+      <View style={styles.down}>
+        <TouchableOpacity style={styles.buttonForm}>
+          <Image source={require('./assets/playButton.png')} style={styles.buttonImage}/>      
+        </TouchableOpacity>
+      </View>
+      
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +29,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  middle: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  down: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  buttonForm: {
+    width: 80,
+    height: 80,
+    borderColor: 'black',
+    borderWidth: 5,
+    borderRadius: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonImage: {
+    height: 20,
+    width: 20,
+  }
 });
+
