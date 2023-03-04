ec27173146e8de36500e048dee904e54a15df37d
lou.broda <lou.broda@etu.uca.fr>
2023-03-04 08:04:24 +0100

    refont page seetings + add button to display flatlist of flag

--- screens/Settings.tsx
+++ screens/Settings.tsx
@@ -11,8 +11,13 @@ export default function SettingsScreen({ navigation }) {
   const { isDarkMode, toggleTheme } = useContext(ColorContext);
   const { dealerName, setDealerName } = useContext(DealerContext);
   const [text, setText] = useState<string>('');
+  const [languageListVisible, setLanguageListVisible] = useState(false);
 
   const switchColor = isDarkMode ? 'white' : '#303030'
+
+  const handleLanguageListToggle = () => {
+    setLanguageListVisible(!languageListVisible);
+  };
 
   const LANGUAGE_LIST = [
     { id: '1', flag: require('../assets/french.png') },
@@ -30,15 +35,18 @@ export default function SettingsScreen({ navigation }) {
 
   const styles = StyleSheet.create({
     settingRow: {
-      flexDirection: 'row',   
+      flexDirection: 'row',
+      alignItems: 'center',
     },
   
     settingRow2: {
       flexDirection: 'row',
     },
   
-    options: { 
-      height: '100%',
+    settings: { 
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
       backgroundColor: isDarkMode ? '#303030' : 'white',
     },
   
@@ -49,57 +57,81 @@ export default function SettingsScreen({ navigation }) {
       padding: 10,
       color: isDarkMode ? 'white' : '#303030',
     },
+
+    icon: {
+      marginLeft: 10,
+      color: switchColor
+    },
+
+    icon2: {
+      marginRight: 10,
+      color: switchColor
+    },
+
+    sectionTitle: {
+      flex: 1,
+      fontSize: 26,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      marginLeft: 10,
+      color: isDarkMode ? 'white' : '#303030'
+    },
+
+    languageListContainer: {
+      height: 120,
+    },
+
+    languageListContent: {
+      paddingHorizontal: 10,
+    },
+
   });
 
   return (
-    <SafeAreaView>
-      <View style={styles.options}>
-
-        <View style={styles.settingRow}>
-          <Icon name='globe-outline' size={50} color={switchColor}></Icon>
-          <Text style={{fontSize: 30, fontWeight: 'bold', color: isDarkMode ? 'white' : '#303030'}}>Language</Text>
-        </View>
-
-        <View>
-          <FlatList
-            horizontal={true}
-            data={LANGUAGE_LIST}
-            renderItem={renderItem}
-            keyExtractor={item => item.id}
-          />
-        </View>
-
-        <View style={styles.settingRow}>
-          <View>
-            <Icon name='moon' size={45} color={switchColor}></Icon>
-          </View>
-          <Text style={{fontSize: 30, fontWeight: 'bold', color: isDarkMode ? 'white' : '#303030'}}>Dark Mode</Text>
-          <Switch value={isDarkMode} onValueChange={toggleTheme} />
-        </View>
-
-        <View style={styles.settingRow}>
-          <Icon name='information-circle-outline' size={50} color={switchColor}></Icon>
-          <Text style={{fontSize: 30, fontWeight: 'bold', color: isDarkMode ? 'white' : '#303030'}}>About us</Text>
-        </View>
-
-        <View style={styles.settingRow2}>
-          <Icon name='pencil' size={50} color={switchColor}></Icon>
-          <Text style={{fontSize: 30, fontWeight: 'bold', color: isDarkMode ? 'white' : '#303030'}}>Name your dealer</Text>
-        </View>
-        
-        <View>
-          <TextInput
-            placeholder="Choose the name of your dealer..."
-            onChangeText={newText => setText(newText)}
-            onSubmitEditing={() => setDealerName(text)}
-            value={text}   
-            style={styles.input}
-            cursorColor={switchColor}
-            placeholderTextColor={switchColor}
-          />
-        </View>
-        
-      </View>
-    </SafeAreaView>
+    <View style={styles.settings}>
+  <View style={styles.settingRow}>
+    <Icon name="globe-outline" size={40} style={styles.icon} />
+    <Text style={styles.sectionTitle}>Language</Text>
+    <TouchableOpacity onPress={handleLanguageListToggle}>
+      <Icon name="arrow-down-circle-outline" size={30} style={styles.icon2} />
+    </TouchableOpacity>
+  </View>
+  {languageListVisible && (
+    <View style={styles.languageListContainer}>
+      <FlatList
+        horizontal={true}
+        data={LANGUAGE_LIST}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        contentContainerStyle={styles.languageListContent}
+      />
+    </View>
+  )}
+    <View style={styles.settingRow}>
+      <Icon name="moon" size={40} style={styles.icon} />
+      <Text style={styles.sectionTitle}>Dark Mode</Text>
+      <Switch value={isDarkMode} onValueChange={toggleTheme}/>
+    </View>
+    <View style={styles.settingRow}>
+      <Icon name="information-circle-outline" size={40} style={styles.icon} />
+      <Text style={styles.sectionTitle}>About us</Text>
+    </View>
+    <View style={styles.settingRow}>
+      <Icon name="pencil" size={40} style={styles.icon} />
+      <Text style={styles.sectionTitle}>Name your dealer</Text>
+    </View>
+    <View>
+        <TextInput
+          placeholder="Choose the name of your dealer..."
+          onChangeText={newText => setText(newText)}
+          onSubmitEditing={() => setDealerName(text)}
+          value={text}   
+          style={styles.input}
+          cursorColor={switchColor}
+          placeholderTextColor={switchColor}
+        />
+    </View>
+  </View>
   );
+
 }
